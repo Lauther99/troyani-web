d8e8f3fd2bea8ec405ae46b867bd7b04f84f43fc
Lauther27 <lautherharold27@gmail.com>
2026-02-19 21:05:04 -0500

    refactor: comment out unused components in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,28 @@ import ProductTypes from "@/components/ProductTypes";
 import WhyTroyani from "@/components/WhyTroyani";
 import Gallery from "@/components/Gallery";
 import CTASection from "@/components/CTASection";
+import HeroScrolled from "@/components/HeroScrolled";
+import ProductTypesClipPath from "@/components/ProductTypesClipPath";
 
 export default function Home() {
   return (
     <>
       {/* Hero Section with animated title */}
-      <Hero />
+      {/* <Hero /> */}
+      <HeroScrolled />
 
       {/* 3D Animation Section - Your GSAP scroll frames */}
       <ScrollFrames frameCount={191} />
 
       {/* Product Types Section - Dark background */}
       <ProductTypes />
+      {/* <ProductTypesClipPath /> */}
 
       {/* Why Choose Troyani - Light background */}
       <WhyTroyani />
 
       {/* Gallery Section - Dark background */}
-      <Gallery />
+      {/* <Gallery /> */}
 
       {/* Final CTA Section - Dark background */}
       <CTASection />
